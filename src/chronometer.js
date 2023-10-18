8786e986622df7a73b87b0af578f8f1d6925b786
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-18 22:39:44 +0200

    labdonerafael.js

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,69 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if(printTimeCallback){
+        clearInterval(this.intervalId)
+      }
+    }, 1000)
+      
+
+    
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = this.currentTime /100/60;
+    let currentMinutes = Math.floor(minutes, Math.random())
+    return currentMinutes
+
   }
 
   getSeconds() {
     // ... your code goes here
-  }
+  //  let seconds = this.intervalId % 60;
+  //  let currentSeconds = Math.floor(seconds, Math.random())
+  //  return currentSeconds
+  //}
+  let seconds = this.currentTime;
+  if(this.currentTime === 0){
+    return 0
+  } 
+  return seconds %60
+}
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let i = value;
+    let text = i.toString()
+    if(text.length === 1){
+      text = "0" + text
+    }
+  
+    return text
+
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
+
   }
 
   split() {
     // ... your code goes here
+
   }
 }
